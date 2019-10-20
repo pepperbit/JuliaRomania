d6d9b986fae69ab2e38962f0f02a3b68177098bc
pepperbit <45738726+pepperbit@users.noreply.github.com>
2019-10-20 12:12:01 +0300

    added resources from event

---
+++ files/6_special_characters.jl
@@ -0,0 +1,13 @@
+cd(raw"C:\Projects\JuliaMeetup\JuliaIntro\Julia_basics")
+"""
+Eval line with Ctrl+Enter
+Eval and step to next line with Shift+Enter
+Hit ] in REPL to enter Package mode
+Hit ? in REPL to search documentation
+"""
+
+""" 6. Special Characters"""
+# type \sigma + TAB
+σ = 1
+μ = 0
+β = 1
