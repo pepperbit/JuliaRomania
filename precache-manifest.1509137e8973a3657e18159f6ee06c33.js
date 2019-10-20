bb907572f8013fddc022e72a0b54ecf1c0640078
pepperbit <pepperbit@yahoo.com>
2019-10-20 12:37:37 +0300

    Updates

---
+++ precache-manifest.1509137e8973a3657e18159f6ee06c33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630b8ce24f2f1478655fae11f5884ab7",
+    "url": "/JuliaRomania/index.html"
+  },
+  {
+    "revision": "b7dd72a62b07d3a1563d",
+    "url": "/JuliaRomania/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "ae189417f3111aa3613e",
+    "url": "/JuliaRomania/static/css/main.831e862a.chunk.css"
+  },
+  {
+    "revision": "b7dd72a62b07d3a1563d",
+    "url": "/JuliaRomania/static/js/2.9b379e7a.chunk.js"
+  },
+  {
+    "revision": "ae189417f3111aa3613e",
+    "url": "/JuliaRomania/static/js/main.941329c4.chunk.js"
+  },
+  {
+    "revision": "70b9503d95ed69c458d5",
+    "url": "/JuliaRomania/static/js/runtime-main.761476f2.js"
+  },
+  {
+    "revision": "f33fcccdbeac0a608eec48483a07161c",
+    "url": "/JuliaRomania/static/media/02_logo.f33fcccd.png"
+  },
+  {
+    "revision": "6f53ab1b42ce4c92bf9f8dec5b52f452",
+    "url": "/JuliaRomania/static/media/files.6f53ab1b.png"
+  },
+  {
+    "revision": "b02bee6d14074a39a8b43f36686f035c",
+    "url": "/JuliaRomania/static/media/header.b02bee6d.png"
+  },
+  {
+    "revision": "e2f7d193919c024e6803129dbc0a1d7d",
+    "url": "/JuliaRomania/static/media/intro.e2f7d193.png"
+  },
+  {
+    "revision": "310a0b1b66831e3b22765ea945f7c081",
+    "url": "/JuliaRomania/static/media/introdl.310a0b1b.png"
+  }
+]);
